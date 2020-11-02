3f0a1f1b57f34b96e1801d2dc6d8fa7e3369677c
Denilson Alves Oliveira <denilson.oliveira@basis.com.br>
2020-11-02 12:01:10 -0300

    001-Arquivos
    arquivo do projeto

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Halloween</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+
+<body class="fundo">
+
+  <div class="wrapper">
+
+    <div class="strips">
+      <div class="left-strip"></div>
+      <div class="middle-strip"></div>
+      <div class="right-strip"></div>
+    </div>
+
+    <div class="logo">
+      <img src="./assets/img/icones.png" alt="halloween">
+    </div>
+
+    <div class="sidetext">
+      <ul>
+        <li>Home</li>
+        <li>Festa</li>
+        <li>Convite</li>
+        <li>Contato</li>
+      </ul>
+    </div>
+
+    <div class="menu-btn">
+      <span class="menu-icon"><i class="fa fa-bars"></i></span>
+    </div>
+
+    <div class="social-media">
+      <ul>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>Twitter</li>
+      </ul>
+    </div>
+
+    <div class="person">
+      <img src="./assets/img/Halloween.png" alt="">
+    </div>
+
+    <div class="content">
+      <p class="animate__animated animate__swing ">Você está pronto para</p>
+      <h1 class="animate__animated animate__swing ">Halloween?</h1>
+      <button class="btn-1">Festa Halloween</button>
+      <button class="btn-2">Book- Álbum</button>
+       <p class="animate__animated animate__shakeX">Denilson Alves</p>
+
+    </div>
+
+
+    <div class="dots">
+      <span class="dot"></span>
+      <span class="dot"></span>
+      <span class="dot"></span>
+      <span class="dot"></span>
+      <span class="dot"></span>
+    </div>
+
+  </div>
+
+  <script src="js/wow.min.js"></script>
+  <script>new WOW().init();</script>
+
+  <script src="assets/js/animacao.js"></script>
+
+</body>
+
+</html>
